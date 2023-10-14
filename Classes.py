16ae3e1084a3cc8e1f453928a3f47f8971776c1e
Simon Khan <simonkhan160@gmail.com>
2023-10-14 23:50:51 +0200

    maj

--- Classes.py
+++ Classes.py
@@ -13,64 +13,81 @@ class Revision():
         self.nom=nom
         self.TabTime=TabTime
 day = datetime.date.today()
-def nbre_jours(year,month):
-    if month in [4, 6, 9, 11]:
-        return(30)
-    elif month in [1, 3, 5, 7, 8, 10, 12]:
-        return(31)
-    else:
-        if year % 400 == 0 or (year % 4 == 0 and year % 100 != 0):
-            return(29)
-        else:
-            return(28)
+
+def conversiontime(secondes):
+    min = int(secondes/60)
+    h = int(min/60)
+    j = int(h/24)
+    mois = int(j/30,436875)
+    restmin = min%60
+    resth = h%24
+    restj = j%30,436875
+    return mois, restj, resth, restmin
+
+
+
+
+
+
 def secondes_a_heures(x, pos):
-    return f"{x/(3600):.2f}"
+    return f"{int(x/(3600))} h {int((x*(3600))/60)} min "
 def secondes_a_jours(x, pos):
-    return f"{x/(3600*24):.2f}"
-def secondes_a_mois(x, pos): #algorithme incorrect
-    return f"{x/(3600*24*nbre_jours(day.year, day.month)):.2f}"
+    return f"{int(x/(3600*24))} j {int((x%(3600*24))/3600)} h"
+def secondes_a_mois(x, pos):
+    return f"{int(x/(3600*24*30.436875))} m {int((x%(3600*24*30.436875))/(3600*24))} j"
+     #Avec 30,436875 la durée moyenne d'un mois
+
 
 
 
 def Ecrire(Matiere):
-    C2 = 4 #Interval n = C2 * Interval n-1
-    C1 = 3600*2 #Durée en seconde avant la première révision
+    C2 = 3 #Interval n = C2 * Interval n-1
+    C1 = 3600*1.5 #Durée en seconde avant la première révision
     K = 0.8 #Pourcentage Minimum avant qu'on passe à la prochaine révision
     colors=['r','g','b', 'c', 'm', 'y', 'k', 'w']
 
     current_time = datetime.datetime.now().timestamp()
     Matiere.TabTime.append(current_time)
+    temps_ecoule = Matiere.TabTime[-1]-Matiere.TabTime[0]
+
+    plt.figure(figsize=(10, 8))
+
 
     for i in range(len(Matiere.TabTime)-1):
         t = np.arange(Matiere.TabTime[i]-Matiere.TabTime[0], Matiere.TabTime[i+1]-Matiere.TabTime[0], 0.1)
         f = 100*np.exp(((t-(Matiere.TabTime[i]-Matiere.TabTime[0]))*np.log(K)/(C1*C2**i)))
         plt.plot(t, f, label=f"Révision n°{i}", color=colors[i])
 
-    x = np.linspace(0, 3600*24*7, 30)
+    x = np.linspace(0, 2*(temps_ecoule), 30)
     seuil=np.ones_like(x)*100*K
     plt.plot(x,seuil,'k--', label = "Seuil")
 
-    timer = (C1*C2**i)-(Matiere.TabTime[i+1]-Matiere.TabTime[0])
+    timer = (C1*C2**i)-(temps_ecoule)
     if timer>0 :
-        plt.text(10, 10, f'Prochaine révision conseillée dans {int(timer)} secondes')
+        plt.text((temps_ecoule/10), 10, f"prochaine révision conseillée dans {conversiontime(int(timer))[0]} mois, {conversiontime(int(timer))[1]} jours, {conversiontime(int(timer))[2]} heures, {conversiontime(int(timer))[3]} minutes ")
     else :
-        plt.text(10, 10, f"Vous avez dépassé l'horaire idéale pour réviser la matière {Matiere.nom}",color='r')
+        plt.text((temps_ecoule/3), 10, f"Vous avez dépassé l'horaire idéale pour réviser la matière {Matiere.nom}",color='r')
 
-    plt.legend()
-
-
-
-    formatterJour = FuncFormatter(secondes_a_jours)
+    plt.legend(loc = 'upper right')
 
 
+    if temps_ecoule < 3600*24 :
+        format = FuncFormatter(secondes_a_heures)
+    elif temps_ecoule < 1.5 * 3600*24*30.436875 : #après 1 mois et demi l'unité de légende du temps devient le mois
+        format = FuncFormatter(secondes_a_jours)
+    else :
+        format = FuncFormatter(secondes_a_mois)
 
-    plt.xlabel('temps (en jours)')
+
+
+
+    plt.xlabel('temps')
     axes = plt.gca()
     #axes.set_xlim(0, 3153600) #on définit l'axe x sur 1 an : 3153600 secondes
     #axes.set_xlim(0, 3600*24*7) #on définit l'axe sur 1 semaine
-    axes.set_xlim(0,2*(Matiere.TabTime[-1]-Matiere.TabTime[0]))
+    axes.set_xlim(0,2*(temps_ecoule))
 
-    axes.xaxis.set_major_formatter(formatterJour)
+    axes.xaxis.set_major_formatter(format)
 
     plt.ylabel('% Memorisé')
     axes.set_ylim(0, 100)
